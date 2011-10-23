0c8a61f76da9e6f5e5a64ceef72f5847928b315a
邵彬 <shaobin0604@163.com>
2011-10-23 17:55:36 +0800

    initial commit

---
+++ jni/Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+
+#
+# Compile libiconv
+# 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libiconv
+LOCAL_SRC_FILES := libiconv/libiconv.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+#
+# Compile libmms
+#
+include $(CLEAR_VARS)
+
+LOGLEVELS := ERROR WARN INFO DEBUG TRACE
+cflags_loglevels := $(foreach ll,$(LOGLEVELS),-DAACD_LOGLEVEL_$(ll))
+
+LOCAL_MODULE    := libmms
+LOCAL_SRC_FILES := libmms-0.6.2/mms.c \
+					libmms-0.6.2/mmsh.c \
+					libmms-0.6.2/mmsx.c \
+					libmms-0.6.2/uri.c
+LOCAL_CFLAGS	:= -DHAVE_CONFIG_H $(cflags_loglevels)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/libmms-0.6.2 $(LOCAL_PATH)/libiconv/include
+LOCAL_SHARED_LIBRARIES := libiconv
+LOCAL_LDLIBS 	:= -llog -lm
+include $(BUILD_SHARED_LIBRARY)
---
+++ src/cn/yo2/aquarium/libmms/Main.java
@@ -0,0 +1,7 @@
+package cn.yo2.aquarium.libmms;
+
+import android.app.Activity;
+
+// just a dummy class
+public class Main extends Activity {
+}
